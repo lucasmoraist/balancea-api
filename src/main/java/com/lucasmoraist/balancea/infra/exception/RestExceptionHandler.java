111f35ac7e948e1b775ed1a713dbd46222fbc57a
lucasmoraist <luksmnt1101@gmail.com>
2024-10-14 15:05:42 -0300

    feat (main): exception handler to authentication

--- src/main/java/com/lucasmoraist/balancea/infra/exception/RestExceptionHandler.java
+++ src/main/java/com/lucasmoraist/balancea/infra/exception/RestExceptionHandler.java
@@ -6,11 +6,14 @@ import com.lucasmoraist.balancea.exceptions.InvalidDateException;
 import com.lucasmoraist.balancea.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.naming.AuthenticationException;
+import java.nio.file.AccessDeniedException;
 import java.util.List;
 
 @RestControllerAdvice
@@ -39,6 +42,26 @@ public class RestExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     protected ResponseEntity<ExceptionDTO> handleResourceNotFoundException(ResourceNotFoundException e) {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    protected ResponseEntity<String> handleBadCredentialsException() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    protected ResponseEntity<String> handleAuthenticationException() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falha na autenticação");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    protected ResponseEntity<String> handleAccessDeniedException() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Acesso negado");
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ExceptionDTO> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ExceptionDTO(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR));
     }
 
     private record DataValidation(String campo, String mensagem) {
