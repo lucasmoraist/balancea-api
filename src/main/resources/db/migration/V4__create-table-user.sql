e6b643f2f7b8f7c5d72a425a5287ce1708483323
lucasmoraist <luksmnt1101@gmail.com>
2024-10-14 15:07:36 -0300

    feat (main): adding migration to create table user

---
+++ src/main/resources/db/migration/V4__create-table-user.sql
@@ -0,0 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS t_user (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
