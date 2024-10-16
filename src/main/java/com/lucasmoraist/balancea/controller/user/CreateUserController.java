7db79d4b1a556ad0eff8eb5e363bbe419f3c0ffa
lucasmoraist <luksmnt1101@gmail.com>
2024-10-16 09:17:05 -0300

    feat (main): removing unused imports

--- src/main/java/com/lucasmoraist/balancea/controller/user/CreateUserController.java
+++ src/main/java/com/lucasmoraist/balancea/controller/user/CreateUserController.java
@@ -6,7 +6,6 @@ import com.lucasmoraist.balancea.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
